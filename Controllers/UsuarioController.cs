730b69e27793aa82993aad346b4da5737864b3d5
Marcos Lopez <marcosleonellopez@gmail.com>
2024-12-12 16:53:17 -0300

    eliminar usuarios por parte del admin

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -8,10 +8,12 @@ using IUsuarioRepo;
 
 namespace trabajo_final.Controllers;
 
-public class UsuarioController : Controller{
+public class UsuarioController : Controller
+{
     private readonly ILogger<UsuarioController> _logger;
     private readonly IUsuarioRepository _usuarioRepository;
-    public UsuarioController(ILogger<UsuarioController> logger, IUsuarioRepository usuarioRepository){
+    public UsuarioController(ILogger<UsuarioController> logger, IUsuarioRepository usuarioRepository)
+    {
         _logger = logger;
         _usuarioRepository = usuarioRepository;
     }
@@ -22,10 +24,84 @@ public class UsuarioController : Controller{
     }
 
     [HttpGet]
-    public IActionResult GetAll(){
+    public IActionResult GetAll()
+    {
         var usuarios = _usuarioRepository.GetAll();
         var viewModel = usuarios.Select(usuario => new DataUsuario(usuario)).ToList();
         return View(viewModel);
     }
-    
+    [HttpGet]
+    public IActionResult GetById(int idUsuario)
+    {
+        var viewModel = new DataUsuario(_usuarioRepository.GetById(idUsuario));
+        if (viewModel == null)
+        {
+            ViewData["ErrorMessage"] = "El usuario con el ID proporcionado no existe.";
+            return View("Error");
+        }
+        return View(viewModel);
+    }
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View(new Usuario());
+    }
+    [HttpPost]
+    public IActionResult Create(Usuario usuario)
+    {
+        if (ModelState.IsValid)
+        {
+            _usuarioRepository.Create(usuario);
+            return RedirectToAction("GetAll");
+        }
+        return View(usuario);
+    }
+    [HttpGet]
+    // solo podria acceder un admin
+    public IActionResult ListToEdit()
+    {
+        var usuarios = _usuarioRepository.GetAll();
+        var viewModel = usuarios.Select(usuario => new DataUsuario(usuario)).ToList();
+        return View(viewModel);
+    }
+    [HttpGet]
+    public IActionResult Delete(int idUsuario)
+    {
+        var usuario = _usuarioRepository.GetById(idUsuario);
+        if (usuario == null)
+        {
+            ViewData["ErrorMessage"] = "El usuario con el ID proporcionado no existe.";
+            return View("Error");
+        }
+        var viewModel = new DataUsuario(usuario);
+        return View(viewModel);
+    }
+    [HttpPost]
+    public IActionResult DeleteConfirmed(int idUsuario)
+    {
+        var usuario = _usuarioRepository.GetById(idUsuario);
+        if (usuario == null)
+        {
+            ViewData["ErrorMessage"] = "El usuario con el ID proporcionado no existe.";
+            return View("Error");
+        }
+        _usuarioRepository.Remove(idUsuario);
+        return RedirectToAction("GetAll");
+    }
+    [HttpGet]
+    public IActionResult Update(int idUsuario){
+        var usuario = _usuarioRepository.GetById(idUsuario);
+        if(usuario == null){
+            ViewData["ErrorMessage"] = "El usuario con el ID proporcionado no existe.";
+            return View("Error");
+        }
+        return View(usuario);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
 }
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
 public class Usuario
 {
     private int id_usuario;
+
     private string nombre_de_usuario;
+
     private string password;
+
     private MisEnums.RolUsuario rol_usuario;
-    public Usuario(){}
-    public Usuario(int id_usuario, string nombre_de_usuario, string password, MisEnums.RolUsuario rol){
+    public Usuario() { }
+    public Usuario(int id_usuario, string nombre_de_usuario, string password, MisEnums.RolUsuario rol)
+    {
         this.id_usuario = id_usuario;
         this.nombre_de_usuario = nombre_de_usuario;
         this.password = password;
@@ -13,7 +18,12 @@ public class Usuario
     }
 
     public int Id_usuario { get => id_usuario; set => id_usuario = value; }
+    [Required(ErrorMessage = "El nombre de usuario es obligatorio.")]
+    [StringLength(50, ErrorMessage = "El nombre de usuario no puede exceder los 50 caracteres.")]
     public string Nombre_de_usuario { get => nombre_de_usuario; set => nombre_de_usuario = value; }
+    [Required(ErrorMessage = "La contraseña es obligatoria.")]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "La contraseña debe tener entre 6 y 100 caracteres.")]
     public string Password { get => password; set => password = value; }
+    [Required(ErrorMessage = "El rol es obligatorio.")]
     public MisEnums.RolUsuario Rol_usuario { get => rol_usuario; set => rol_usuario = value; }
 }
