4a4e834979f1ef0a1494b4b87f91f34f32454e4a
Marcos Lopez <marcosleonellopez@gmail.com>
2025-01-26 10:13:27 -0300

    logica de vistas con ViewComponents

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pendiente
+* Meodificar la vista del tablero para utilizar ViewComponents
+* revisar anotaciones en codigo
--- viewComponents/MenuUsuarioViewComponent.cs
+++ viewComponents/MenuUsuarioViewComponent.cs
@@ -1,24 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-
-public class MenuViewComponent : ViewComponent
+public class MenuUsuarioViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke(bool loggeado, string rolUsuario)
+    public IViewComponentResult Invoke(string rolUsuario)
     {
-        if (!loggeado)
-        {
-            // Si no está logueado, devuelve la vista general.
-            return View("Default");
-        }
-
         // Selecciona la vista según el rol del usuario.
         return rolUsuario switch
         {
             var rol when rol == MisEnums.RolUsuario.Administrador.ToString() => View("Administrador"),
             var rol when rol == MisEnums.RolUsuario.Operador.ToString() => View("Operador"),
-            _ => View("Default")
+            _ => View("Operador") // no tengo una vista default
         };
 
     }
 }
-
-
